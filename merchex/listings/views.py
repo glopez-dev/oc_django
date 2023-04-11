e6dfbe176ace2f3b923acaa40695df24e1a0ae6b
glopez <glopez-dev@proton.me>
2023-04-11 21:58:53 +0200

    Création du template de base de l'application

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
+# Importe le modèle des groupes de musiques :
+from listings.models import Band, Listing
 from django.shortcuts import render
 
 
+def hello(request):
+    bands = Band.objects.all()
+    return render(request, 'listings/hello.html', {'bands': bands})
 
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1>')
+
+
+def listings(request):
+    listings = Listing.objects.all()
+    return render(request, 'listings/listings.html', {'listings': listings})
+
+
+def contact(request):
+    return render(request, 'listings/contact.html')
